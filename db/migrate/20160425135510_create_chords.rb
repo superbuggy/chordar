c0c8c5e72a562a5c712bdb9637cdf0d829485eb8
Spicy Legato <jpqreichard@gmail.com>
2016-04-26 09:23:58 -0400

    updates migration with proper associations

--- db/migrate/20160425135510_create_chords.rb
+++ db/migrate/20160425135510_create_chords.rb
@@ -2,8 +2,8 @@ class CreateChords < ActiveRecord::Migration
   def change
     create_table :chords do |t|
       t.integer :voices
-      t.string :type
-      t.references :note, index: true, foreign_key: true
+      t.string :quality
+      t.string :root_note
 
       t.timestamps null: false
     end
