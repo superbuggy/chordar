ef9d4168b837f20ac67d9f8885c23e27adabf2ab
Spicy Legato <jpqreichard@gmail.com>
2016-04-27 18:58:09 -0400

    creates a reference chord for notes show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Note.destroy_all
+Chord.destroy_all
 
-c_maj_chord = Chord.create!( voices:3, quality:"major" )
+thee_chord = Chord.create!(voices: 12, quality:"dodecaphonic")
 
-c_maj_note1 = Note.create!(name:"C", letter:"C" ,accidental:"", octave:4, pitch:0, chord_id: c_maj_chord.id )
-c_maj_note2 = Note.create!(name:"E",letter:"E",accidental:"", octave:4, pitch:0, chord_id: c_maj_chord.id )
-c_maj_note3 = Note.create!(name:"G",letter:"G",accidental:"", octave:4, pitch:0, chord_id: c_maj_chord.id )
+note_c       = Note.create!(name:"C",   letter:"C",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+note_c_sharp = Note.create!(name:"C#",   letter:"C",   accidental:"#", octave:4, pitch:0, chord_id: thee_chord.id )
+note_d       = Note.create!(name:"D",   letter:"D",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+note_d_sharp = Note.create!(name:"D#",   letter:"D",   accidental:"#", octave:4, pitch:0, chord_id: thee_chord.id )
+note_e       = Note.create!(name:"E",   letter:"E",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+note_f       = Note.create!(name:"F",   letter:"F",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+note_f_sharp = Note.create!(name:"F#",   letter:"F",   accidental:"#", octave:4, pitch:0, chord_id: thee_chord.id )
+note_g       = Note.create!(name:"G",   letter:"G",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+note_g_sharp = Note.create!(name:"G#",   letter:"G",   accidental:"#", octave:4, pitch:0, chord_id: thee_chord.id )
+note_a       = Note.create!(name:"A",   letter:"A",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+note_a_sharp = Note.create!(name:"A#",   letter:"A",   accidental:"#", octave:4, pitch:0, chord_id: thee_chord.id )
+note_b       = Note.create!(name:"B",   letter:"B",   accidental:"",   octave:4, pitch:0, chord_id: thee_chord.id )
+
+c_maj = Chord.create!(root_note:"C", quality:"major")
+c_maj.construct_chord
+f_maj = Chord.create!(root_note:"F", quality:"major")
+f_maj.construct_chord
+g_maj = Chord.create!(root_note:"G", quality:"major")
+g_maj.construct_chord
