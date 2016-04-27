a018c8d9f03aba195dc7c14f88c83e442eac1c21
Spicy Legato <jpqreichard@gmail.com>
2016-04-27 18:25:40 -0400

    refactors chord building

--- app/models/chord.rb
+++ app/models/chord.rb
@@ -5,26 +5,30 @@ class Chord < ActiveRecord::Base
     self.notes.length
   end
 
+  # each chord type, represented in the string and the corresponding number
+  # of half-steps, relative to the root, for each upper voice
+  def self.schemata
+    {
+      "major" =>      [4, 7],
+      "minor" =>      [3, 7],
+      "diminished" => [3, 6],
+      "augmented" =>  [4, 8],
+      "major7" =>     [4, 7, 11],
+      "minor7" =>     [3, 7, 10],
+      "dom7" =>       [4, 7, 10],
+      "min-maj7" =>    [3, 7, 11]
+    }
+  end
 
+  #this method is to be called after a chord has been saved, and creates Note
+  #objects that make up the chord
   def construct_chord
 
     starting_note_index = Note.tones.index( self.root_note )
     note_indexes_for_building_chord = []
     note_indexes_for_building_chord.push( starting_note_index )
 
-    if self.quality == "major"
-      semitones_from_root = [4, 7]
-    elsif self.quality == "minor"
-      semitones_from_root = [3, 7]
-    elsif self.quality == "major7"
-      semitones_from_root = [4, 7, 11]
-    elsif self.quality == "minor7"
-      semitones_from_root = [3, 7, 10]
-    elsif self.quality == "dom7"
-      semitones_from_root = [4, 7, 10]
-    elsif self.quality == "minmaj7"
-      semitones_from_root = [3, 7, 11]
-    end
+    semitones_from_root = Chord.schemata[ self.quality ]
 
     semitones_from_root.each do |voice_halfsteps|
       next_voice = ( voice_halfsteps + starting_note_index ) % 12
@@ -40,7 +44,6 @@ class Chord < ActiveRecord::Base
                          chord_id: self.id
             )
     end
-
 
   end
 
