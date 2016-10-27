3adca60351c41fc9f669fedcfaa81450a7df915c
James Reichard <jpqreichard@gmail.com>
2016-10-27 18:58:17 -0400

    add vanilla object constructor for Notes, and broken frequency calculation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,24 +1,32 @@
-class Note < ActiveRecord::Base
-  belongs_to :chord
+class Note
 
   # TODO implement math for generating a pitch in Hz
-  # TODO implement octave functionality
 
-  def self.tone_book
-    {
-        "B#"  => 0,  "C"  => 0,   "Dbb" => 0,
-        "B##" => 1,  "C#" => 1,   "Db" =>  1,
-        "C##" => 2,  "D"  => 2,   "Ebb" => 2,
-        "D#"  => 3,  "Eb" => 3,   "Fbb" => 3,
-        "D##" => 4,  "E"  => 4,   "Fb"  => 4,
-        "E#"  => 5,  "F"  => 5,   "Gbb" => 5,
-        "E##" => 6,  "F#" => 6,   "Gb"  => 6,
-        "F##" => 7,  "G"  => 7,   "Abb" => 7,
-        "G#"  => 8,  "Ab" => 8,
-        "G##" => 9,  "A"  => 9,   "Bbb" => 9,
-        "A#"  => 10, "Bb" => 10,  "Cbb" => 10,
-        "A##" => 11, "B"  => 11,  "Cb"  => 11
+  def initialize (letter, accidental, octave)
+    # Reference: Middle C (C4) has a pitch_index of 48
+    accidentals = {
+      "bb": -2,
+      "b": -1,
+      "n": 0,
+      "#": 1,
+      "##": 2
     }
-  end
 
+    tone_letters = {
+      "C": 0,
+      "D": 2,
+      "E": 4,
+      "F": 5,
+      "G": 7,
+      "A": 9,
+      "B": 11
+    }
+
+    pitch_index = tone_letters[letter.to_sym] + accidentals[accidental.to_sym] + (octave * 12)
+
+    @letter = letter
+    @accidental = accidental
+    @pitch_index = pitch_index
+    @frequency = 2 ** ( (pitch_index-69) / 12) * 440
+  end
 end
